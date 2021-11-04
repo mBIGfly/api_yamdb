80bdffa429d0764fe4ee6e325d651442aefdc388
hi-ais <86742603+hi-ais@users.noreply.github.com>
2021-11-04 19:57:01 +0300

    add apps:api, review, create 1st version models

---
+++ api_yamdb/review/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Title, Review, Comments
+
+
+admin.site.register(Title)
+admin.site.register(Review)
+admin.site.register(Comments)
---
+++ api_yamdb/review/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+
+User = get_user_model()
+
+
+def validate_rate(value):
+    if value not in [0, 10]:
+        raise ValidationError('Оценка должна быть от 0 до 10!')
+
+
+class Title(models.Model):
+    # author = models.ForeignKey(User)
+    name = models.CharField(max_length=100)
+    # review = models.ForeignKey(Review)
+    # rating = models.ForeignKey(Rating)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.text
+
+
+class Comments(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.text
+
+
+class Rating(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+    score = models.IntegerField(validators=[validate_rate])
+
+    def __str__(self):
+        return self.score
