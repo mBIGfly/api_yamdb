b09827995c793305fb919735db2cc099120d51ba
mBIGfly <84184582+mBIGfly@users.noreply.github.com>
2021-11-26 10:57:16 +0300

    reviews pep8

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,11 +1,9 @@
 from django.contrib.auth import get_user_model
-
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
-from reviews.models import Category, Comment, Genre, Review, Title, User
-
 from .validators import username_is_not_me, username_is_unique
+from reviews.models import Category, Comment, Genre, Review, Title, User
 
 User = get_user_model()
 
